5c62e4854dc44220f3af3005a363bf2c8f1670fa
LahiruShamika <159902795+LahiruShamika@users.noreply.github.com>
2025-02-06 10:36:06 +0530

    Create CRUD Part

--- CRUDAPP/Program.cs
+++ CRUDAPP/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CRUDAPP.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<CRUDAPPContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CRUDAPPContext") ?? throw new InvalidOperationException("Connection string 'CRUDAPPContext' not found.")));
 
 var app = builder.Build();
 
